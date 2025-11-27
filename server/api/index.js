3ac1bb263973d6a1a2771e8929602512e175d4e8
Spectra <shaikhfujail4444@gmail.com>
2025-11-27 13:04:44 +0530

    updated index

--- server/api/index.js
+++ server/api/index.js
@@ -23,6 +23,10 @@ const app = express();
 app.use(
   cors({
     origin: [
+      "http://localhost:3000",
+      "http://72.61.227.110:3000",
+      "http://72.61.227.110",
+      "https://make-mee-cosmatics.vercel.app",
       "https://makemee.in",
       "https://www.makemee.in"   // optional but recommended
     ],
