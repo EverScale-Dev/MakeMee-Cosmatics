39dded39f44af67466619ddc4834a386f7458e7c
vallabh sathe <vallabhsathe9906@gmail.com>
2026-01-14 09:24:12 +0530

    frontend

--- server/api/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { contactForm } = require('../controllers/contactController');
-
-const router = express.Router();
-
-// POST /contact
-router.post('/', contactForm);
-
-module.exports = router;
